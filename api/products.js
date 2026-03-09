2d501868eff1647236b97ef6fda737da51f970ea
cadastrovalfre-cmd <cadastrovalfre@gmail.com>
2026-03-09 10:16:04 -0300

    Refactor product handler for better readability

--- api/products.js
+++ api/products.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
 
-const page = req.query.page || 1
+  const page = req.query.page || 1;
 
-const response = await fetch(
-`https://1225878.commercesuite.com.br/web_api/products?limit=50&page=${page}`
-)
+  const response = await fetch(
+    `https://1225878.commercesuite.com.br/web_api/products?limit=50&page=${page}`
+  );
 
-const data = await response.json()
+  const data = await response.json();
 
-const products = data.Products.map(p => p.Product)
+  const products = data.Products.map(p => ({
+      id: p.Product.id,
+      name: p.Product.name,
+      price: p.Product.price,
+      brand: p.Product.brand,
+      slug: p.Product.slug
+  }));
 
-res.status(200).json(products)
+  res.status(200).json(products);
 
 }
