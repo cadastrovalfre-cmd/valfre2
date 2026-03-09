b31b0cceee35fda80087711b8e2ac2696b018b00
cadastrovalfre-cmd <cadastrovalfre@gmail.com>
2026-03-09 11:02:31 -0300

    Refactor product handler to streamline product mapping

--- api/products.js
+++ api/products.js
@@ -1,32 +1,33 @@
-export default async function handler(req, res) {
+const products = data.Products.map(p => ({
+  id: Number(p.Product.id),
 
-  const page = req.query.page || 1;
+  name: p.Product.name || "",
 
-  const response = await fetch(
-    `https://1225878.commercesuite.com.br/web_api/products?limit=50&page=${page}`
-  );
+  price: Number(p.Product.price) || 0,
 
-  const data = await response.json();
+  promotional_price: Number(p.Product.promotional_price) || 0,
 
-  const products = data.Products.map(p => ({
-      id: Number(p.Product.id),
-      name: p.Product.name || "",
-      price: Number(p.Product.price) || 0,
-      promotional_price: Number(p.Product.promotional_price) || 0,
-      brand: p.Product.brand || "",
-      category_id: Number(p.Product.category_id) || 0,
-      stock: Number(p.Product.stock) || 0,
-      description: p.Product.description || "",
-      description_small: p.Product.description_small || "",
-      model: p.Product.model || "",
-      weight: p.Product.weight || "",
-      warranty: p.Product.warranty || "",
-      image:
-  p.Product.ProductImage?.[0]?.https ||
-  p.Product.ProductImage?.[0]?.http ||
-  p.Product.image ||
-  null
-  }));
+  brand: p.Product.brand || "",
 
-  res.status(200).json(products);
-}
+  category_id: Number(p.Product.category_id) || 0,
+
+  stock: Number(p.Product.stock) || 0,
+
+  description: p.Product.description || "",
+
+  description_small: p.Product.description_small || "",
+
+  model: p.Product.model || "",
+
+  weight: p.Product.weight || "",
+
+  warranty: p.Product.warranty || "",
+
+  ean: p.Product.ean || "",
+
+  image:
+    p.Product.ProductImage?.[0]?.https ||
+    p.Product.ProductImage?.[0]?.http ||
+    p.Product.image ||
+    null
+}));
