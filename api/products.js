606248889b9967d92c006eb6687f0a29f214e0c0
cadastrovalfre-cmd <cadastrovalfre@gmail.com>
2026-03-09 10:58:46 -0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -9,13 +9,20 @@ export default async function handler(req, res) {
   const data = await response.json();
 
   const products = data.Products.map(p => ({
-      id: p.Product.id,
-      name: p.Product.name,
-      price: p.Product.price,
-      brand: p.Product.brand,
-      slug: p.Product.slug
+      id: Number(p.Product.id),
+      name: p.Product.name || "",
+      price: Number(p.Product.price) || 0,
+      promotional_price: Number(p.Product.promotional_price) || 0,
+      brand: p.Product.brand || "",
+      category_id: Number(p.Product.category_id) || 0,
+      stock: Number(p.Product.stock) || 0,
+      description: p.Product.description || "",
+      description_small: p.Product.description_small || "",
+      model: p.Product.model || "",
+      weight: p.Product.weight || "",
+      warranty: p.Product.warranty || "",
+      image: p.Product.ProductImage?.[0]?.https || null
   }));
 
   res.status(200).json(products);
-
 }
