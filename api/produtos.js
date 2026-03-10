b5fc90cae49b999fd58e21c3eda753eb269fa565
cadastrovalfre-cmd <cadastrovalfre@gmail.com>
2026-03-10 17:54:34 -0300

    Update produtos.js

--- api/produtos.js
+++ api/produtos.js
@@ -75,6 +75,8 @@ export default async function handler(req, res) {
 
         warranty: p.warranty || "",
 
+        reference: p.reference || p.ean || "",
+
         ean: p.ean || "",
 
         slug: p.slug || "",
