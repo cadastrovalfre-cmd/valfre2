0786fe6ddac2cd58b64c8ae1b838ef20cf48d6b4
cadastrovalfre-cmd <cadastrovalfre@gmail.com>
2026-03-11 16:48:29 -0300

    Set default Tiny API token in config

--- api/_lib/config.js
+++ api/_lib/config.js
@@ -1,6 +1,8 @@
 const config = {
   tinyBaseUrl: process.env.TINY_API_BASE_URL || 'https://api.tiny.com.br/api2',
-  tinyToken: process.env.TINY_API_TOKEN || '',
+  tinyToken:
+    process.env.TINY_API_TOKEN ||
+    '2ca5c90d4de5985b2ebbe364345aef554deb91181599868d6c965c0c1e37723c',
   tinyDefaultFormato: 'json',
   mpBaseUrl: process.env.MERCADO_PAGO_API_BASE_URL || 'https://api.mercadopago.com',
   mpAccessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN || '',
